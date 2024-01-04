a6535e25b682b2131dc06810151c52045c462fe9
abdussamiakanda <abdussamiakanda@gmail.com>
2024-01-04 02:53:34 -0600

    .

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,5 +1,6 @@
 function myHeader() {
   document.getElementById("header").innerHTML = `<div class="menu">
+        <i class="fa-solid fa-bars bars" onclick="handleMenu('o')"></i>
         <div class="head" onclick="goTo('./')">
             <img src="./assets/image/icon.png" alt="">
         </div>
@@ -22,9 +23,30 @@ function myHeader() {
             <div class="menu-item" onclick="goTo('./support')">Support</div>
             <div class="menu-item" onclick="goTo('./contact')">Contact</div>
         </div>
+    </div>
+    <div class="moblie-menu" id="moblie-menu">
+        <i class="fa-solid fa-xmark cross" onclick="handleMenu('c')"></i>
+        <div class="menu-item">About</div>
+        <div class="menu-item1" onclick="goTo('./story')">Our Story</div>
+        <div class="menu-item1" onclick="goTo('./charter')">Institutional Charter</div>
+        <div class="menu-item1" onclick="goTo('./facilities')">Research Facilities</div>
+        <div class="menu-item">Research</div>
+        <div class="menu-item1" onclick="goTo('./groups')">Research Groups</div>
+        <div class="menu-item1" onclick="goTo('./publications')">Publications</div>
+        <div class="menu-item" onclick="goTo('./outreach')">Outreach</div>
+        <div class="menu-item" onclick="goTo('./people')">People</div>
+        <div class="menu-item" onclick="goTo('./support')">Support</div>
+        <div class="menu-item" onclick="goTo('./contact')">Contact</div>
     </div>`;
 }
 
+function handleMenu(val) {
+    if (val === 'o')  {
+        document.getElementById('moblie-menu').style.display = 'block';
+    } else {
+        document.getElementById('moblie-menu').style.display = 'none';
+    }
+}
 function goTo(path) {
   window.location.assign(path);
 }
