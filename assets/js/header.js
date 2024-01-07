03725f749495fe93dbbb35dc04950980c7b40077
abdussamiakanda <abdussamiakanda@gmail.com>
2024-01-07 02:16:09 -0600

    .

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,42 +1,42 @@
 function myHeader() {
   document.getElementById("header").innerHTML = `<div class="menu">
         <i class="fa-solid fa-bars bars" onclick="handleMenu('o')"></i>
-        <div class="head" onclick="goTo('./')">
-            <img src="./assets/image/icon.png" alt="">
+        <div class="head" onclick="goTo('./../../../')">
+            <img src="./../../../assets/image/icon.png" alt="">
         </div>
         <div class="pc-menu">
             <div class="menu-item about">About <i class="fa-solid fa-chevron-down"></i>
                 <div class="about-items">
-                    <div onclick="goTo('./story')">Our Story</div>
-                    <div onclick="goTo('./charter')">Institutional Charter</div>
-                    <div onclick="goTo('./facilities')">Research Facilities</div>
+                    <div onclick="goTo('./../../../story')">Our Story</div>
+                    <div onclick="goTo('./../../../charter')">Institutional Charter</div>
+                    <div onclick="goTo('./../../../facilities')">Research Facilities</div>
                 </div>
             </div>
             <div class="menu-item research">Research <i class="fa-solid fa-chevron-down"></i>
                 <div class="research-items">
-                    <div onclick="goTo('./groups')">Research Groups</div>
+                    <div onclick="goTo('./../../../groups')">Research Groups</div>
                     <div onclick="goTo('./publications')">Publications</div>
                 </div>
             </div>
-            <div class="menu-item" onclick="goTo('./outreach')">Outreach</div>
-            <div class="menu-item" onclick="goTo('./people')">People</div>
-            <div class="menu-item" onclick="goTo('./support')">Support</div>
-            <div class="menu-item" onclick="goTo('./contact')">Contact</div>
+            <div class="menu-item" onclick="goTo('./../../../outreach')">Outreach</div>
+            <div class="menu-item" onclick="goTo('./../../../people')">People</div>
+            <div class="menu-item" onclick="goTo('./../../../support')">Support</div>
+            <div class="menu-item" onclick="goTo('./../../../contact')">Contact</div>
         </div>
     </div>
     <div class="moblie-menu" id="moblie-menu">
         <i class="fa-solid fa-xmark cross" onclick="handleMenu('c')"></i>
         <div class="menu-item">About</div>
-        <div class="menu-item1" onclick="goTo('./story')">Our Story</div>
-        <div class="menu-item1" onclick="goTo('./charter')">Institutional Charter</div>
-        <div class="menu-item1" onclick="goTo('./facilities')">Research Facilities</div>
+        <div class="menu-item1" onclick="goTo('./../../../story')">Our Story</div>
+        <div class="menu-item1" onclick="goTo('./../../../charter')">Institutional Charter</div>
+        <div class="menu-item1" onclick="goTo('./../../../facilities')">Research Facilities</div>
         <div class="menu-item">Research</div>
-        <div class="menu-item1" onclick="goTo('./groups')">Research Groups</div>
-        <div class="menu-item1" onclick="goTo('./publications')">Publications</div>
-        <div class="menu-item" onclick="goTo('./outreach')">Outreach</div>
-        <div class="menu-item" onclick="goTo('./people')">People</div>
-        <div class="menu-item" onclick="goTo('./support')">Support</div>
-        <div class="menu-item" onclick="goTo('./contact')">Contact</div>
+        <div class="menu-item1" onclick="goTo('./../../../groups')">Research Groups</div>
+        <div class="menu-item1" onclick="goTo('./../../../publications')">Publications</div>
+        <div class="menu-item" onclick="goTo('./../../../outreach')">Outreach</div>
+        <div class="menu-item" onclick="goTo('./../../../people')">People</div>
+        <div class="menu-item" onclick="goTo('./../../../support')">Support</div>
+        <div class="menu-item" onclick="goTo('./../../../contact')">Contact</div>
     </div>`;
 }
 
