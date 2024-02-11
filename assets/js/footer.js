638ac71a1298440875e3a95d9bc7fb5ae7eeb84d
abdussamiakanda <abdussamiakanda@gmail.com>
2024-02-10 22:22:57 -0600

    new design of the website

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,11 +1,32 @@
 function myFooter() {
-  document.getElementById("footer").innerHTML = `<div class="social-div">
-        <p>
-        Copyright © 2024 Institution for Theoretical and Computational Physics Research.
-        <br>
-        All rights reserved.
-        </p>
-    </div>`;
+  document.getElementById("footer").innerHTML = `<div class="academic2">
+    <div class="links">
+      <div class="footer-div">
+        <b>Contact Info</b>
+        <span class="bar"></span>
+        Email: <a href="mailto:info@itcpr.org" target="_blank">info@itcpr.org</a> <br>
+        URL: <a href="./../../../contact">itcpr.org/contact</a>
+      </div>
+      <div class="footer-div">
+        <b>Resources</b>
+        <span class="bar"></span>
+        <a href="./../../../newsletters">ITCPR Newsletter</a><br>
+        <a href="./../../../charter">Institutional Charter</a>
+      </div>
+      <div class="footer-div">
+        <b>Related Links</b>
+        <span class="bar"></span>
+        <a href="./../../../groups">Research Groups</a><br>
+        <a href="./../../../outreach">Outreach</a>
+      </div>
+    </div>
+  </div>
+  <p>
+    Copyright © 2024 Institution for Theoretical and Computational Physics
+    Research.
+    <br />
+    All rights reserved.
+  </p>`;
 }
 
 myFooter();
