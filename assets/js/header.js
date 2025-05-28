6f419735854ed2e9d884fe2f49a46d72a0ab5958
abdussamiakanda <abdussamiakanda@gmail.com>
2025-05-28 18:38:10 -0500

    .

--- assets/js/header.js
+++ assets/js/header.js
@@ -3,10 +3,11 @@ function myHeader() {
     <div class="top-bar">
       <div class="container">
         <div class="top-bar-right">
-          <a href="https://twitter.com/itcpr" target="_blank"><i class="fab fa-twitter"></i></a>
-          <a href="https://linkedin.com/company/itcpr" target="_blank"><i class="fab fa-linkedin"></i></a>
+          <a href="https://www.linkedin.com/company/itcpr-org" target="_blank"><i class="fab fa-linkedin"></i></a>
           <a href="https://github.com/itcpr" target="_blank"><i class="fab fa-github"></i></a>
           <a href="https://www.facebook.com/itcpr.org" target="_blank"><i class="fab fa-facebook"></i></a>
+          <a href="https://www.youtube.com/@itcpr-org" target="_blank"><i class="fab fa-youtube"></i></a>
+          <a href="/rss.xml" target="_blank"><i class="fas fa-rss"></i></a>
         </div>
         <div class="top-bar-left">
           <a href="/internships">APPLY</a>
