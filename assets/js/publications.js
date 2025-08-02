12d95c8d752d5d5ee5369917feddf8837ffc12c9
Md Abdus Sami Akanda <abdussamiakanda@gmail.com>
2025-08-02 17:20:11 -0500

    .

--- assets/js/publications.js
+++ assets/js/publications.js
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         const pubElement = document.createElement('div');
         pubElement.className = 'publication-card';
         pubElement.innerHTML = `
-            <div class="publication-tags"><span>${pub.type}</span> <span>${pub.group}</span></div>
+            <div class="publication-tags"><span class="${pub.type.replaceAll(' ', '-').toLowerCase()}">${pub.type}</span> <span>${pub.group}</span></div>
             <div class="publication-details">${pub.authors}, <b>${pub.title}</b>, ${pub.journal}, (${pub.month} ${pub.year})</div>
             <a href="${pub.doi}" class="publication-link" target="_blank">View details...</a>
         `;
